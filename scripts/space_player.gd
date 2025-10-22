cc13a498150f37b277476645fd1a217746f33cb7
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2025-10-22 03:28:23 -0700

    Moved towards prototype
    Made more of level and added player animations. Tried to add gravity shifting but that system is hard, will need to work on it more.

--- scripts/space_player.gd
+++ scripts/space_player.gd
@@ -1,25 +1,86 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -300.0
+const MAX_SPEED = 200.0
+const ACCELERATION = 10.0
+const JUMP_VELOCITY = -500.0
+const PLAYER_GRAVITY = 980.0
+const ROTATION_SPEED = 10.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+enum GravityDirection { DOWN, UP, RIGHT, LEFT }
+var GravOrientation = { GravityDirection.DOWN: 0, GravityDirection.RIGHT: -90, 
+						GravityDirection.LEFT: 90, GravityDirection.UP: 180}
+
+var in_air: bool = false
+var PlayerGravity = GravityDirection.DOWN
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Get input direction: -1, 0, 1 (-1 when left, 1 when right)
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Add the gravity. Will change with PlayerGravity state
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		match PlayerGravity:
+			GravityDirection.DOWN:
+				velocity.y += PLAYER_GRAVITY * delta
+				rotation = move_toward(rotation, GravOrientation[PlayerGravity], 
+										ROTATION_SPEED)
+		if not in_air:
+			in_air = true
+			animated_sprite.play("jump_start")
+	else:
+		match PlayerGravity:
+			GravityDirection.UP:
+				velocity.y -= PLAYER_GRAVITY * delta
+				rotation = move_toward(rotation, GravOrientation[PlayerGravity], 
+										ROTATION_SPEED)
+			GravityDirection.RIGHT:
+				velocity.x += PLAYER_GRAVITY * delta
+				rotation = move_toward(rotation, GravOrientation[PlayerGravity], 
+										ROTATION_SPEED)
+			GravityDirection.LEFT:
+				velocity.x -= PLAYER_GRAVITY * delta
+				rotation = move_toward(rotation, GravOrientation[PlayerGravity], 
+										ROTATION_SPEED)
+		if in_air:
+			in_air = false
+			animated_sprite.play("jump_end")
+		
+		#supposed to only play once jump end animation is done, this doesn't 
+		#work and it would require to much time to complete right now
+		if animated_sprite.animation_finished:
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# :DEBUG: change gravity with ijkl, will be hooked to random timer later
+	if Input.is_action_just_pressed("grav_up"):
+		PlayerGravity = GravityDirection.UP
+	if Input.is_action_just_pressed("grav_down"):
+		PlayerGravity = GravityDirection.DOWN
+	if Input.is_action_just_pressed("grav_left"):
+		PlayerGravity = GravityDirection.LEFT
+	if Input.is_action_just_pressed("grav_right"):
+		PlayerGravity = GravityDirection.RIGHT
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Apply movement
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if PlayerGravity == GravityDirection.DOWN:
+			velocity.x = move_toward(velocity.x, 0, MAX_SPEED)
 
 	move_and_slide()
